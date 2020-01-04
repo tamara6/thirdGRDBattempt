768a77cbbc290566e08beb10bafd7f016a26cdd9
Tamara Snyder <tsnyder@uark.edu>
2020-01-03 20:15:22 -0600

    Initial Commit

---
+++ firstGRDBattempt/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  firstGRDBattempt
+//
+//  Created by Tamara Snyder on 1/3/20.
+//  Copyright © 2020 Tamara Snyder. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
